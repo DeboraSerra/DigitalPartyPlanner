9a28e5a57790d843f3b087ba31d19c52372b8b39
Debora Serra <debora.r.serra@gmail.com>
2025-04-04 19:25:52 -0700

    style: add style to form

--- party_form.php
+++ party_form.php
@@ -4,6 +4,36 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <style>
+      form {
+        border: 1px solid #999;
+        border-radius: 4px;
+        padding: 12px 8px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 12px;
+        max-width: 500px;
+        margin: 128px auto 0;
+      }
+
+      label {
+        display: flex;
+        gap: 4px;
+        width: 100%;
+      }
+
+      button {
+        padding: 8px 16px;
+        font-size: 16px;
+        background-color: #710000;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        width: 100%;
+      }
+    </style>
   </head>
   <body>
     <form action="process.php" method="post">
